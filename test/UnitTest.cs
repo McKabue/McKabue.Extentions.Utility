f8078fa677f336e5a04d1ea8fcb3f90944704622
Kabue Charles <mckabue@gmail.com>
2019-08-29 22:52:42 +0300

    tests

---
+++ test/UnitTest.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Common_C_Sharp_Utility_Methods.Tests
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
