e01bed909c003649915f4a5d0ea33a94e665662f
Kabue Charles <me@mckabue.com>
2019-10-06 00:43:56 +0300

    String to Bytes

--- src/Strings.cs
+++ src/Strings.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Common_C_Sharp_Utility_Methods
 {
     public static class Strings
@@ -5,5 +7,15 @@ namespace Common_C_Sharp_Utility_Methods
         public static bool HasValue(this string val) => !string.IsNullOrEmpty(val) && !string.IsNullOrWhiteSpace(val);
 
         public static bool IsEmpty(this string val) => !HasValue(val);
+
+        public static byte[] ToBytes(this string value, Encoding encoding = null)
+        {
+            if (value.HasValue())
+            {
+                return (encoding ?? Encoding.UTF8)?.GetBytes(value) ?? new byte[] { };
+            }
+
+            return new byte[] { };
+        }
     }
 }
