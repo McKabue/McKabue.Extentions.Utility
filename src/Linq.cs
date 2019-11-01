151115596870312d23f4fc813e5b382f778fa686
Kabue Charles <me@mckabue.com>
2019-11-01 23:49:36 +0300

    Predicate Distinct

---
+++ src/Linq.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace Common_C_Sharp_Utility_Methods
+{
+    public static class Linq
+    {
+        /// <summary>
+        /// https://stackoverflow.com/a/489421/3563013
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <returns></returns>
+        public static IEnumerable<TSource> Distinct<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
+    }
+}
