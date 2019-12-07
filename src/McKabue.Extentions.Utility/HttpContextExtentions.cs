92ce2508ce844b099a394bb3a8bfe5753fe57c25
Kabue Charles <me@mckabue.com>
2019-12-07 23:51:07 +0300

    LINQ

---
+++ src/McKabue.Extentions.Utility/HttpContextExtentions.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace McKabue.Extentions.Utility
+{
+    public static class HttpContextExtentions
+    {
+        public static bool IsAuthenticated(this HttpContext _httpContext)
+        {
+            return _httpContext?.User != null &&
+                (_httpContext?.User?.Identities?.Any(i => i.IsAuthenticated) ?? false);
+        }
+        public static string AbsolutePath(this HttpContext _httpContext)
+        {
+            return _httpContext != null ?
+                UrlCombineLib.UrlCombine.Combine(_httpContext?.BasePath(), _httpContext?.RelativePath()) :
+                string.Empty;
+        }
+        public static string RelativePath(this HttpContext _httpContext)
+        {
+            return _httpContext?.Request != null ?
+                UriHelper.GetEncodedPathAndQuery(_httpContext?.Request) :
+                string.Empty;
+        }
+        public static string BasePath(this HttpContext _httpContext)
+        {
+            return _httpContext != null ? 
+                $"{_httpContext?.Request?.Scheme}://{_httpContext?.Request?.Host.Value}" : 
+                string.Empty;
+        }
+        public static bool IsLocalhost(this HttpContext _httpContext)
+        {
+            string host = _httpContext?.Request?.Host.Host.Trim();
+            string ip = _httpContext?.IpAddress();
+            return string.Equals(host, "localhost", StringComparison.OrdinalIgnoreCase) || 
+                string.Equals(ip, "::1", StringComparison.OrdinalIgnoreCase) || 
+                string.Equals(ip, "127.0.0.1", StringComparison.OrdinalIgnoreCase) || 
+                string.Equals(ip, "127.0.0.0", StringComparison.OrdinalIgnoreCase) || 
+                string.Equals(ip, "0.0.0.0", StringComparison.OrdinalIgnoreCase);
+        }
+        public static string Referrer(this HttpContext _httpContext)
+        {
+            return _httpContext.Request.Headers["Referer"].ToString();
+        }
+        public static string UserAgent(this HttpContext _httpContext)
+        {
+            return _httpContext.Request.Headers["User-Agent"].ToString();
+        }
+        public static string IpAddress(this HttpContext _httpContext)
+        {
+            return _httpContext?.Connection?.RemoteIpAddress.ToString();
+        }
+        public static bool IsAjax(this HttpContext _httpContext)
+        {
+            return _httpContext?.Request?.Headers["X-Requested-With"].ToString() == "XMLHttpRequest";
+        }
+    }
+}
--- src/McKabue.Extentions.Utility/Linq.cs
+++ src/McKabue.Extentions.Utility/Linq.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace McKabue.Extentions.Utility
 {
@@ -23,6 +24,63 @@ namespace McKabue.Extentions.Utility
                     yield return element;
                 }
             }
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/a/14160879
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="dict"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static U Get<T, U>(this IDictionary<T, U> dict, T key, U _default = default(U))
+        {
+            U val = _default;
+            dict.TryGetValue(key, out val);
+            return val;
+        }
+
+        public static U Get<T, U>(this IEnumerable<KeyValuePair<T, U>> dict, T key, U _default = default(U), Func<T, T, bool> comparer = null)
+        {
+            KeyValuePair<T, U>? val = dict?.FirstOrDefault(k => comparer?.Invoke(k.Key, key) ?? k.Key.Equals(key));
+            return val.HasValue ? val.Value.Value : _default;
+        }
+
+        public static U Get<T, U>(this IEnumerable<KeyValuePair<T, U>> dict, Func<T, bool> comparer, U _default = default(U))
+        {
+            KeyValuePair<T, U>? val = dict?.FirstOrDefault(k => comparer?.Invoke(k.Key) ?? false);
+            return val.HasValue ? val.Value.Value : _default;
+        }
+
+
+        public static void Set<T, U>(this IDictionary<T, U> dict, T key, U val)
+        {
+            dict[key] = val;
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/a/1177590/3563013
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dict"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static bool AddIfNone<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue value)
+        {
+            return !(dict?.Keys?.Any(k => k.Equals(key)) ?? false) && (dict?.TryAdd(key, value) ?? false);
+        }
+
+        public static bool AddIfNone<T, K>(this List<T> items, T item, Func<T, K> keyComparer = null)
+        {
+            bool isInItems = keyComparer != null
+                ? items.Any(k => keyComparer(k).Equals(keyComparer(item)))
+                : items.Any(k => k.Equals(item));
+
+            if (!isInItems)
+                items.Add(item);
+            return isInItems;
         }
     }
 }
